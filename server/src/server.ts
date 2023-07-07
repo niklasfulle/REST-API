65a613fbe3e4e9bf7f1fcc13d47d704c5757ca41
Niklas Fulle <36989748+Salkin263@users.noreply.github.com>
2023-07-07 16:27:12 +0200

    user auth works

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,10 +1,13 @@
-import express from 'express';
-import cookieParser from 'cookie-parser';
-import cors from 'cors';
-import helmet from 'helmet';
-import { connect, disconnect } from './utils/database';
-import logger from './utils/logger';
-import { CORS_ORIGIN } from './constans';
+import express from "express";
+import cookieParser from "cookie-parser";
+import cors from "cors";
+import { connect, disconnect } from "./utils/database";
+import logger from "./utils/logger";
+import { CORS_ORIGIN } from "./constants";
+import helmet from "helmet";
+import userRoute from "./modules/user/user.route";
+import authRoute from "./modules/auth/auth.route";
+import deserializeUser from "./middleware/deserializeUser";
 
 const PORT = process.env.PORT || 4000;
 
@@ -12,34 +15,38 @@ const app = express();
 
 app.use(cookieParser());
 app.use(express.json());
-app.use(cors({
-  origin: CORS_ORIGIN,
-  credentials: true,
+app.use(
+  cors({
+    origin: CORS_ORIGIN,
+    credentials: true,
+  })
+);
+app.use(helmet());
+app.use(deserializeUser);
 
-}));
-app.use(helmet())
-
+app.use("/api/users", userRoute);
+app.use("/api/auth", authRoute);
 const server = app.listen(PORT, async () => {
   await connect();
-  logger.info(`Server is running on port ${PORT}`);
+  logger.info(`Server listening at htp://localhost:${PORT}`);
 });
 
-const signals: NodeJS.Signals[] = ['SIGINT', 'SIGTERM'];
+const signals = ["SIGTERM", "SIGINT"];
 
 function gracefulShutdown(signal: string) {
   process.on(signal, async () => {
+    logger.info("Goodbye, got signal", signal);
+    server.close();
 
-    logger.info(`Received ${signal}. Shutting down gracefully...`);
-    // close server
-    server.close()
-
-    // close database
+    // disconnect from the db
     await disconnect();
+
+    logger.info("My work here is done");
 
     process.exit(0);
   });
 }
 
-signals.forEach((signal) => {
-  gracefulShutdown(signal);
-});
+for (let i = 0; i < signals.length; i++) {
+  gracefulShutdown(signals[i]);
+}
